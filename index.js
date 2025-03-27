691840c352bb3694598ce156dd06a5ffc8532ef3
piotrek <piotrek@ubuntu.com>
2025-03-27 09:51:12 +0100

    add REST API

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from 'url';
 const app = express();
 const port = 5000;
 const current_folder  = dirname(fileURLToPath(import.meta.url));
+const master_key='gumissek';
 let postsList=[];
 
 
@@ -88,6 +89,102 @@ app.post('/editPost',(req,res)=>{
     res.redirect('/');
 });
 
+
+// OWN API
+
+// GET ALL POSTS
+app.get('/allPosts',(req,res)=>{
+    if (postsList.length>0) {
+        res.status(200).send(postsList);
+    } else {
+        res.status(404).send({error:'There are no posts'});
+    }
+});
+
+// GET post by id
+
+app.get('/post/:id',(req,res)=>{
+    let post_id = req.params.id;
+    let requested_post ='';
+
+    for (let index = 0; index < postsList.length; index++) {
+        const post = postsList[index];
+        if (postsList[index].id==post_id) {
+            requested_post=postsList[index];
+        }
+    };
+
+    if (requested_post!='') {
+        res.status(200).send(requested_post)
+    }else{
+        res.status(404).send({error:`Post with id:${post_id} doesnt exist`})
+    }
+});
+
+
+// POST add new post
+
+app.post('/post/add',(req,res)=>{
+    if (req.body['title']&&req.body['description']&&req.body['author']) {
+        let new_post = new postConstructor(generateId(postsList.length),req.body['title'],req.body['description'],req.body['author']);
+        postsList.push(new_post);
+        res.status(200).send({success:'New post has been added'});
+    } else {
+        res.status(404).send({error:'No complete data provided'})
+    }
+    
+});
+
+// PATCH edit existing post
+app.patch('/post/:id',(req,res)=>{
+    let post_id = req.params.id;
+    let requested_post='';
+
+    for (let index = 0; index < postsList.length; index++) {
+        const post = postsList[index];
+        if (post.id==post_id) {
+            for(let key in req.body){
+                post[key]=req.body[key];
+            }
+            requested_post=post;
+        }
+    };
+    if (requested_post!='') {
+        res.status(200).send(requested_post)
+    }else{
+        res.status(404).send({error:`Post with id:${post_id} doesnt exist`})
+    }
+});
+
+// DELETE post by id
+app.delete('/post/:id',(req,res)=>{
+    let post_id = req.params.id;
+    let requested_post_index='';
+
+    for (let index = 0; index < postsList.length; index++) {
+        const post = postsList[index];
+        if (post.id==post_id) {
+            requested_post_index=parseInt(index);
+        }
+    }
+    if (requested_post_index!=='') {
+        postsList.splice(requested_post_index,1);
+        res.status(200).send({success:`Post with id:${post_id} has been removed`})
+    }else{
+        res.status(404).send({error:`Post with id:${post_id} doesnt exist in database`})
+    }
+});
+
+// DELETE all posts 
+app.delete('/allPosts',(req,res)=>{
+    if (req.body.key===master_key) {
+        postsList=[]
+        res.status(200).send({success:'All posts have been removed'})
+    } else {
+        res.status(404).send({error:'Not authorized'})
+    }
+});
+
 app.listen(port,()=>{
-    console.log(`app is working on port: ${port}`)
+    console.log(`app is working on : http://localhost:${port}`)
 });
